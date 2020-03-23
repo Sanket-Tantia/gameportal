0a21d51a726e1a20cba16c60b539eb85b3c021d5
Tantia <satantia@teksystems.com>
2020-03-23 15:13:09 +0530

    create_user dashboard UI min changes, allowed all hosts, requirements.txt new linting packages

--- game_portal/settings.py
+++ game_portal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rp(#fmf*-pby%*rk8fb4rf5=oro_(06gk*#)nfpze75wa9&obo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
